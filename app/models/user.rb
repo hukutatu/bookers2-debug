2d4f2159d3536bd0a67998ff28996be10a6583ba
hukutatu <127284708+hukutatu@users.noreply.github.com>
2023-05-02 07:50:23 +0000

    fifth

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,10 +9,12 @@ class User < ApplicationRecord
    has_one_attached :profile_image
    has_many :favorites, dependent: :destroy
    has_many :book_comments, dependent: :destroy
+   
    has_many :follower, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
-   has_many :follower_user, through: :followed, source: :follower
-   has_many :followed, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
    has_many :following_user, through: :follower, source: :followed
+
+   has_many :followed, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+   has_many :follower_user, through: :followed, source: :follower
 
 
 
@@ -33,8 +35,22 @@ class User < ApplicationRecord
    end
 
   #フォローしていればtrueを返す
-   def following?(user)
+  def following?(user)
     following_user.include?(user)
-  end	 
+  end
+  
+  def self.looks(search, word)
+    if search == "perfect_match"
+      @user = User.where("name LIKE?", "#{word}")
+    elsif search == "forward_match"
+      @user = User.where("name LIKE?","#{word}%")
+    elsif search == "backward_match"
+      @user = User.where("name LIKE?","%#{word}")
+    elsif search == "partial_match"
+      @user = User.where("name LIKE?","%#{word}%")
+    else
+      @user = User.all
+    end
+  end
 
 end
